5adcf03383e738b651eb4da7ce17af76aa5d6548
Klaus Wenger <kjwenger@yahoo.com>
2021-09-22 17:08:43 +0200

    more styles

---
+++ src/main/java/io/github/kjwenger/HafenrichterDemonstration.java
@@ -0,0 +1,17 @@
+package io.github.kjwenger;
+
+public class HafenrichterDemonstration {
+
+    private int    intField    = 1;
+    private long   longField   = 1L;
+    private String stringField = "String";
+
+    public HafenrichterDemonstration(int intField,
+                                     long longField,
+                                     String stringField) {
+        this.intField    = intField;
+        this.longField   = longField;
+        this.stringField = stringField;
+    }
+
+}
